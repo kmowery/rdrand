5db8bccaf080ca10190994569f52d12aa1b4d5b1
kmowery <kmowery@gmail.com>
2013-09-22 15:49:39 -0700

    Add RDRAND array-filling function

--- rdrand.c
+++ rdrand.c
@@ -9,30 +9,54 @@
 #define bit_RDRAND 0x40000000
 #endif
 
-
 int cpu_supports_rdrand() {
   uint32_t eax, ebx, ecx, edx;
   __get_cpuid(1, &eax, &ebx, &ecx, &edx);
   return ecx & bit_RDRAND;
 }
 
+int rdrand_fill_array(uint64_t* array, int size) {
+  // We want to do this as quickly as possible. Assembly hoy!
+  uint64_t successes = 0;
+  uint64_t temp = 0;
+
+  __asm volatile(
+      "top_of_rdrand_loop:"
+      // "jump if ecx == 0"
+      "jecxz end_of_rdrand_loop ;"
+
+      "rdrand %1 ;"
+      "adc $0, %0 ;"
+
+      "mov %1, (%4) ; "
+      "add $8, %4 ; "
+
+      "dec %3 ;"
+      "jmp top_of_rdrand_loop ;"
+
+      "end_of_rdrand_loop:"
+    : "=r" (successes), "=r" (temp)
+    : "0" (successes), "c" (size), "r"(array)
+    );
+
+  return (int)successes;
+}
+
 int main(int argc, char** argv) {
-
-  uint64_t x;
-  unsigned char success;
-
   if(!cpu_supports_rdrand()) {
     printf("Error: Your CPU does not support RDRAND.\n");
     exit(1);
   }
 
-  asm volatile("rdrand %0 ; setc %1"
-    : "=r" (x), "=qm" (success));
+  int size = 0;
+  uint64_t array[16];
 
-  if(success) {
-    printf("rdrand successful: %" PRIx64 "\n", x);
-  } else {
-    printf("rdrand failed: %" PRIx64 "\n", x);
+  size = rdrand_fill_array(array, 16);
+
+  printf("rdrand successful: %d\n", size);
+
+  for(int i = 0; i < size; i++) {
+    printf("%" PRIx64 "\n", array[i]);
   }
 
   return 0;
